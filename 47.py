32fc09be0a360818fa923d17b95f5565551dc552
Rohit Parit <rohitparit70@gmail.com>
2025-07-10 22:52:47 +0530

    Print All Prime Numbers in a Range

---
+++ 47.py
@@ -0,0 +1,15 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+start = int(input("Enter start of range: "))
+end = int(input("Enter end of range: "))
+
+print("Prime numbers between", start, "and", end, "are:")
+for num in range(start, end + 1):
+    if is_prime(num):
+        print(num, end=' ')
