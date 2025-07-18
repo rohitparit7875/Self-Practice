b818e9457c30ae4f19c4c78239d00c6cd65eb00c
Rohit Parit <rohitparit70@gmail.com>
2025-07-18 22:10:52 +0530

    Dice Rolling Simulator

---
+++ Minigame4.py
@@ -0,0 +1,8 @@
+import random
+
+while True:
+    input("Press Enter to roll the dice...")
+    print(f"You rolled: {random.randint(1, 6)}")
+    again = input("Roll again? (y/n): ").lower()
+    if again != 'y':
+        break
