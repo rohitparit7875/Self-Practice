2bd732669c2d9b97de2d3e7b21f4933a03b6d0d8
Rohit Parit <rohitparit70@gmail.com>
2025-07-20 22:55:56 +0530

    motivate_me

---
+++ 69.py
@@ -0,0 +1,12 @@
+import random
+
+quotes = [
+    "Believe in yourself and all that you are.",
+    "Stay positive, work hard, make it happen.",
+    "Do something today that your future self will thank you for.",
+    "Success doesn't come to you. You go to it.",
+    "Start where you are. Use what you have. Do what you can."
+]
+
+print("\n💬 Motivation for today:\n")
+print(random.choice(quotes))
