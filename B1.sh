f4be7a9aa384a7dfccd7274b61f2f694ba6dcaf1
Rohit Parit <rohitparit70@gmail.com>
2025-09-01 12:29:03 +0530

    B1.sh
    Addition

---
+++ B1.sh
@@ -0,0 +1,11 @@
+# Simple program to add two numbers
+
+# take input from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# add the numbers
+sum_result = num1 + num2
+
+# display result
+print("The sum is:", sum_result)
