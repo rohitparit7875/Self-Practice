dd4ed9ba22d432301f5fedf0a8a4fc040238d8c2
Rohit Parit <rohitparit70@gmail.com>
2026-01-02 00:36:43 +0530

    Create Rev. number .c

---
+++ Rev. number .c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num, rev = 0;
+
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+    while (num != 0) {
+        rev = rev * 10 + num % 10;
+        num = num / 10;
+    }
+
+    printf("Reversed number = %d", rev);
+    return 0;
+}
